cb58780d39d65d186d22f866ecbc17cf30c9cf44
Gabriel Lievano <gl@gabriellievano.com>
2021-11-12 12:12:58 -0800

    add zsh config to myrkheim

--- hosts/myrkheim/default.nix
+++ hosts/myrkheim/default.nix
@@ -10,6 +10,15 @@
   programs.ssh.startAgent = true;
   services.openssh.startWhenNeeded = true;
 
+  # TODO: move to zsh module
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableGlobalCompInit = false;
+    promptInit = "";
+  };
+
   networking = {
     networkmanager.enable = true;
     interface.wlp0s20f3.useDHCP = false;
@@ -18,5 +27,18 @@
       allowedTCPPorts = [];
       allowedUDPPorts = [];
     };
+  };
+
+  user.packages = with pgks; [
+    exa
+    nix-zsh-completions
+    zsh
+  ];
+
+  env = {
+    ZDOTDIR = "$XDG_CONFIG_HOME/zsh";
+    ZSH_CACHE = "$XDG_CACHE_HOME/zsh";
+    ZGEN_DIR = "$XDG_DATA_HOME/zsh";
+    ZGEN_SOURCE = "$ZGEN_DIR/zgen.zsh";
   };
 }
