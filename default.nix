9ff1101d95b60a7b1b4a7daf56268ea481067d51
Gabriel Lievano <gl@gabriellievano.com>
2021-12-04 09:28:07 -0800

    dpi suddenly all works

--- default.nix
+++ default.nix
@@ -52,11 +52,11 @@ with lib.my;
   environment.variables.DOTFILES_BIN = config.dotfiles.binDir;
   environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
 
-  services.xserver.dpi = 192;
+  services.xserver.dpi = 138;
   environment.variables = {
-    GDK_SCALE = "1";
-    GDK_DPI_SCALE = "0.5";
-    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2.5";
+    # GDK_SCALE = "1";
+    # GDK_DPI_SCALE = "0.5";
+    # _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2.5";
   };
 
   # the bare minimum
