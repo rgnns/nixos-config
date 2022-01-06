72558d2d1975869d056c017be1992b2d5ca55d87
Gabriel Lievano <jglievano@gmail.com>
2022-01-06 00:40:31 +0000

    hardward-configuration for manx

--- hosts/manx/default.nix
+++ hosts/manx/default.nix
@@ -63,6 +63,8 @@
   programs.ssh.startAgent = true;
   services.openssh.startWhenNeeded = true;
 
+  services.xserver.libinput.enable = true;
+
   # TODO: move to bluetooth module
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
