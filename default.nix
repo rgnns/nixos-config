88132ab8accb5d88190bab4ea79cf1272f8bf792
Gabriel Lievano <gl@gabriellievano.com>
2022-01-18 16:53:04 -0800

    try different dpi

--- default.nix
+++ default.nix
@@ -52,13 +52,13 @@ with lib.my;
   environment.variables.DOTFILES_BIN = config.dotfiles.binDir;
   environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
 
-  services.xserver.dpi = 138;
-  environment.variables = {
-    GDK_SCALE = "1.5";
-    GDK_DPI_SCALE = "0.75";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2.5";
-  };
+  # services.xserver.dpi = 138;
+  # environment.variables = {
+  #   GDK_SCALE = "1.5";
+  #   GDK_DPI_SCALE = "0.75";
+  #   QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+  #   _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2.5";
+  # };
 
   # the bare minimum
   environment.systemPackages = with pkgs; [
